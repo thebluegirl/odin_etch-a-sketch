33969f535209bbba3b048ea33406f781552a37d0
Funto <funto.afuwape@gmail.com>
2022-11-10 19:12:16 +0100

    Create a column for the grid
    A column of cells has been rendered. There are difficulties rendering rows for each column and a different approach may be used to accomplish this.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,34 @@
 //Take control of HTML elements
 const container = document.getElementById('container');
+const columnCells = document.getElementsByClassName('grid-column')
+
+let column;
+let rows;
+
+function makeGrid() {
+    makeColumn(16);
+    makeRows(16);
+}
+
+function makeColumn(num) {
+   for (let i = 0; i < num; i++){
+    column = document.createElement('div');
+    column.setAttribute('class', 'grid-column');
+    column.setAttribute('id', `grid-column-${i}`);
+    container.appendChild(column);
+   }
+}
+
+/* function makeRows(num){
+    for (let i = 0; i < Array.from(columnCells).length; i++){
+        for (let j = 0; j < num; j++){
+            rows = document.createElement('div');
+            rows.setAttribute('class', 'grid-rows');
+            rows.setAttribute('id', `grid-row-${j}`);
+            column[j].appendChild(rows)
+        }
+    }
+} */
 
 
+makeGrid();
